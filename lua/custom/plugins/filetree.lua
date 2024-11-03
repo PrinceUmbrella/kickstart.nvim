5efdfe480b012bde4a2e9e7bd7aacb71979bdc2c
Leoul <33269725+PrinceUmbrella@users.noreply.github.com>
2024-11-02 20:46:43 -0700

    Update filetree.lua

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -6,7 +6,16 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
+  config = function()
     require('neo-tree').setup {}
+    
+    -- Define mappings for all modes
+    vim.keymap.set('n', 'ntc', ':Neotree close<CR>', { silent = true }) -- Normal mode
+    vim.keymap.set('i', 'ntc', '<ESC>:Neotree close<CR>', { silent = true }) -- Insert mode
+    vim.keymap.set('v', 'ntc', '<ESC>:Neotree close<CR>', { silent = true }) -- Visual mode
+    
+    vim.keymap.set('n', 'nto', ':Neotree open<CR>', { silent = true }) -- Normal mode
+    vim.keymap.set('i', 'nto', '<ESC>:Neotree open<CR>', { silent = true }) -- Insert mode
+    vim.keymap.set('v', 'nto', '<ESC>:Neotree open<CR>', { silent = true }) -- Visual mode
   end,
 }
